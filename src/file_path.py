0de513ec370e238a4bdb5a67dfce04f01afcf535
Shuting He <shutingmelloge@gmail.com>
2024-06-10 08:47:25 -0700

    update file path

--- src/file_path.py
+++ src/file_path.py
@@ -1,1 +1,2 @@
-mh_file = 'data/raw_data/500_Cities__City-level_Data__GIS_Friendly_Format___2017_release_20240514.csv'
+mh_file = '../data/raw_data/500_Cities__City-level_Data__GIS_Friendly_Format___2017_release_20240514.csv'
+mh_cleaned = '../data/cleaned_data/mh_cleaned.csv'
